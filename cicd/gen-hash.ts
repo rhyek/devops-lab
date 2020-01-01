df53e293f1c3101d3725214181992acee67ec030
Carlos González <carlos.rgn@gmail.com>
2020-01-01 11:09:58 -0600

    folder-hash sha256, hex

--- cicd/gen-hash.ts
+++ cicd/gen-hash.ts
@@ -2,6 +2,9 @@ import path from 'path';
 
 const { hashElement } = require('folder-hash');
 
-hashElement(path.resolve(__dirname, `../src/${process.env.LERNA_PACKAGE_NAME}/build`), {}).then((hash: any) => {
+hashElement(path.resolve(__dirname, `../src/${process.env.LERNA_PACKAGE_NAME}/build`), {
+  algo: 'sha256',
+  encoding: 'hex',
+}).then((hash: any) => {
   console.log(`BUILD_HASH: ${hash.hash}`);
 });
