d45987cbea084cc503d62438507ff196c5d8f2c0
Carlos González <carlos.rgn@gmail.com>
2020-01-09 13:04:23 -0600

    some cleanup

--- apps/web/src/components/todos/index.tsx
+++ apps/web/src/components/todos/index.tsx
@@ -26,7 +26,7 @@ export default function Todos() {
 
   return (
     <Main>
-      <Title>TODOS</Title>
+      <Title>Todos</Title>
       <TodosContext.Provider value={{ todos, load }}>
         <TodosList />
       </TodosContext.Provider>
